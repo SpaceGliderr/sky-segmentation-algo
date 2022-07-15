179a398e4dd35a8d7ada5660beb80dc463165914
Nilch <46506221+SpaceGliderr@users.noreply.github.com>
2022-07-15 21:31:21 +0800

    Fix

--- src/performance.py
+++ src/performance.py
@@ -47,146 +47,91 @@ def evaluate(algorithm):
     """
     Evaluates the performance of any algorithm on a dataset.
     """
-    dir_name = "10917-samples"
+    dir_name = "test-samples"
     sample_dir_names = next(walk(f'./{dir_name}'), (None, None, []))[1] # Gets all the subdirectories in the target sample directory.
 
-    # for curr_imgs_dir_name in sample_dir_names:
-    curr_imgs_dir_name = "10917"
-    print("Currently evaluating: ", curr_imgs_dir_name)
+    for curr_imgs_dir_name in sample_dir_names:
+        print("Currently evaluating: ", curr_imgs_dir_name)
 
-    imgs_filenames = next(walk(f'./{dir_name}/{curr_imgs_dir_name}'), (None, None, []))[2] # Gets all the filenames in the target image set directory.
-    # print(imgs_filenames)
-    # imgs_filenames = next(walk(f'./{dir_name}/4232'), (None, None, []))[2] # Gets all the filenames in the target image set directory.
-    # imgs_filenames.sort(key=lambda x: (int(x.split('.')[0].split('_')[0]), int(x.split('.')[0].split('_')[1]))) # Sorts image filenames in ascending order
-    
-    # Performance metrics
-    day_list = []
-    night_list = []
+        imgs_filenames = next(walk(f'./{dir_name}/{curr_imgs_dir_name}'), (None, None, []))[2] # Gets all the filenames in the target image set directory.
 
-    days, nights = 0, 0
+        # Performance metrics
+        day_list = []
+        night_list = []
 
-    for curr_img_filename in imgs_filenames:
-        # Read selected image
-        curr_img_path = f'./{dir_name}/{curr_imgs_dir_name}/{curr_img_filename}'
-        # curr_img_path = f'./{dir_name}/4232/{curr_img_filename}'
-        # print(curr_img_path)
+        days, nights = 0, 0
 
-        with open(curr_img_path, 'rb') as f:
-            check_chars = f.read()[-2:]
+        for curr_img_filename in imgs_filenames:
+            # Read selected image
+            curr_img_path = f'./{dir_name}/{curr_imgs_dir_name}/{curr_img_filename}'
 
-        if check_chars != b'\xff\xd9':
-            print("Skipping incomplete image")
-        else:
-            # print(curr_img_filename)
-            curr_img = cv2.imread(curr_img_path, cv2.IMREAD_COLOR)
-            # print(curr_img.shape)
+            with open(curr_img_path, 'rb') as f:
+                check_chars = f.read()[-2:]
 
-            _, day = detector.calculate_boundary_threshold(curr_img, config.row_percentage, config.day_night_threshold)
-
-            # Run algorithm on selected image
-            mask, _ = algorithm(curr_img)
-            # print(mask.shape)
-            mask = cv2.cvtColor(mask, cv2.COLOR_BGR2GRAY)
-            # print(set(mask.flatten()))
-
-            # cv2.imwrite(f'results/4232/{curr_img_filename}', mask)
-            cv2.imwrite(f'results/{curr_imgs_dir_name}/{curr_img_filename}', mask)
-
-            # total_list.extend(curr_img.flatten())
-            # totals += 1
-
-            if day:
-                day_list.extend(mask.flatten())
-                days += 1
+            if check_chars != b'\xff\xd9':
+                print("Skipping incomplete image")
             else:
-                night_list.extend(mask.flatten())
-                nights += 1
+                curr_img = cv2.imread(curr_img_path, cv2.IMREAD_COLOR)
 
-    # print(np.array(day_list).shape)
-    # print(days)
-    # print(np.array(night_list).shape)
-    # print(nights)
+                _, day = detector.calculate_boundary_threshold(curr_img, config.row_percentage, config.day_night_threshold)
 
-    # Get ground truth image
-    ground_truth = cv2.imread(f'./ground_truths/{curr_imgs_dir_name}.png', cv2.IMREAD_GRAYSCALE)
-    # ground_truth = cv2.imread(f'./ground_truths/4232.png', cv2.IMREAD_GRAYSCALE)
-    # print(ground_truth.flatten().shape)
+                # Run algorithm on selected image
+                mask, _ = algorithm(curr_img)
+                mask = cv2.cvtColor(mask, cv2.COLOR_BGR2GRAY)
 
-    day_gt_list = []
-    day_gt_list.extend(repeat(ground_truth.flatten(), days))
+                cv2.imwrite(f'results/{curr_imgs_dir_name}/{curr_img_filename}', mask) # Log results
 
-    night_gt_list = []
-    night_gt_list.extend(repeat(ground_truth.flatten(), nights))
-
-    # total_gt_list = []
-    # total_gt_list.extend(repeat(ground_truth.flatten(), totals))
-
-    # print(np.array(day_gt_list).flatten().shape)
-    # print(np.array(night_gt_list).flatten().shape)
-
-    day = np.array(day_list)
-    night = np.array(night_list)
-    # total = np.array(total_list)
-
-    day_gt = np.array(day_gt_list)
-    night_gt = np.array(night_gt_list)
-    # total_gt = np.array(total_gt_list)
-
-    # print(set(day.flatten()) - set(day_gt.flatten()))
-
-    print("Calculating Performance")
-
-    dtn, dfp, dfn, dtp = generate_confusion_matrix(day_gt, day)
-    print(f'Days: {days} - TN:{dtn} FP:{dfp} FN:{dfn} TP:{dtp}')
-
-    ntn, nfp, nfn, ntp = generate_confusion_matrix(night_gt, night)
-    print(f'Night: {nights} - TN:{ntn} FP:{nfp} FN:{nfn} TP:{ntp}')
-
-    # total = np.concatenate((day.flatten(), night.flatten()))
-    # total_gt = np.concatenate((day_gt.flatten(), night_gt.flatten()))
-    totals = days + nights
-
-    # ttn, tfp, tfn, ttp = generate_confusion_matrix(total_gt, total)
-
-    # day_classification_report = generate_classification_report(day_gt, day)
-    # print("Day Classification Report\n", day_classification_report)
-
-    # night_classification_report = generate_classification_report(night_gt, night)
-    # print("Night Classification Report\n", night_classification_report)
-
-    # ttn, tfp, tfn, ttp = generate_confusion_matrix(total_gt, total)
-    # total_classification_report = generate_classification_report(total_gt, total)
+                if day:
+                    day_list.extend(mask.flatten())
+                    days += 1
+                else:
+                    night_list.extend(mask.flatten())
+                    nights += 1
 
 
-    print(f'Total: {totals} - TN:{dfn + ntn} FP:{dfp + nfp} FN:{dfn + nfn} TP:{dtp + ntp}')
-    # print("Total Classification Report\n", total_classification_report)
+        # Get ground truth image
+        ground_truth = cv2.imread(f'./ground_truths/{curr_imgs_dir_name}.png', cv2.IMREAD_GRAYSCALE)
+
+        day_gt_list = []
+        day_gt_list.extend(repeat(ground_truth.flatten(), days))
+
+        night_gt_list = []
+        night_gt_list.extend(repeat(ground_truth.flatten(), nights))
+
+        day = np.array(day_list)
+        night = np.array(night_list)
+
+        day_gt = np.array(day_gt_list)
+        night_gt = np.array(night_gt_list)
+
+        print("Calculating Performance")
+
+        dtn, dfp, dfn, dtp = generate_confusion_matrix(day_gt, day)
+        print(f'Days: {days} - TN:{dtn} FP:{dfp} FN:{dfn} TP:{dtp}')
+
+        ntn, nfp, nfn, ntp = generate_confusion_matrix(night_gt, night)
+        print(f'Night: {nights} - TN:{ntn} FP:{nfp} FN:{nfn} TP:{ntp}')
+
+        totals = days + nights
+
+        print(f'Total: {totals} - TN:{dtn + ntn} FP:{dfp + nfp} FN:{dfn + nfn} TP:{dtp + ntp}')
 
 
 def evaluate_results():
-    # dir_name = "small-test-samples"
-    # sample_dir_names = next(walk(f'./{dir_name}'), (None, None, []))[1] # Gets all the subdirectories in the target sample directory.
-
-    # for curr_imgs_dir_name in sample_dir_names:
     results_dir_name = "10917"
     dir_name = f'results/{results_dir_name}'
-    print("Currently evaluating: ", dir_name)
 
     imgs_filenames = next(walk(f'./{dir_name}'), (None, None, []))[2] # Gets all the filenames in the target image set directory.
-    # imgs_filenames = next(walk(f'./{dir_name}/4232'), (None, None, []))[2] # Gets all the filenames in the target image set directory.
     imgs_filenames.sort(key=lambda x: (int(x.split('.')[0].split('_')[0]), int(x.split('.')[0].split('_')[1]))) # Sorts image filenames in ascending order
 
     # Performance metrics
-    day_list = []
-    night_list = []
     total_list = []
 
-    days, nights, total = 0, 0, 0
+    total = 0, 0, 0
 
     for curr_img_filename in imgs_filenames:
         # Read selected image
         curr_img_path = f'./{dir_name}/{curr_img_filename}'
-        # curr_img_path = f'./{dir_name}/4232/{curr_img_filename}'
-        # print(curr_img_path)
 
         with open(curr_img_path, 'rb') as f:
             check_chars = f.read()[-2:]
@@ -196,55 +141,12 @@ def evaluate_results():
         else:
             print(curr_img_filename)
             curr_img = cv2.imread(curr_img_path, cv2.IMREAD_GRAYSCALE)
-            # print(curr_img.shape)
 
             total_list.extend(curr_img.flatten())
             total += 1
 
-            # _, day = detector.calculate_boundary_threshold(curr_img, config.row_percentage, config.day_night_threshold)
-
-            # # Run algorithm on selected image
-            # mask, _ = algorithm(curr_img)
-            # # print(mask.shape)
-            # mask = cv2.cvtColor(mask, cv2.COLOR_BGR2GRAY)
-            # # print(set(mask.flatten()))
-
-            # # cv2.imwrite(f'results/4232/{curr_img_filename}', mask)
-            # cv2.imwrite(f'results/{curr_imgs_dir_name}/{curr_img_filename}', mask)
-
-            # if day:
-            #     day_list.extend(mask.flatten())
-            #     days += 1
-            # else:
-            #     night_list.extend(mask.flatten())
-            #     nights += 1
-
-    # print(np.array(day_list).shape)
-    # print(days)
-    # print(np.array(night_list).shape)
-    # print(nights)
-
     # Get ground truth image
     ground_truth = cv2.imread(f'./ground_truths/{results_dir_name}.png', cv2.IMREAD_GRAYSCALE)
-    # ground_truth = cv2.imread(f'./ground_truths/4232.png', cv2.IMREAD_GRAYSCALE)
-    # print(ground_truth.flatten().shape)
-
-    # day_gt_list = []
-    # day_gt_list.extend(repeat(ground_truth.flatten(), days))
-
-    # night_gt_list = []
-    # night_gt_list.extend(repeat(ground_truth.flatten(), nights))
-
-    # # print(np.array(day_gt_list).flatten().shape)
-    # # print(np.array(night_gt_list).flatten().shape)
-
-    # day = np.array(day_list)
-    # night = np.array(night_list)
-
-    # day_gt = np.array(day_gt_list)
-    # night_gt = np.array(night_gt_list)
-
-    # print(set(day.flatten()) - set(day_gt.flatten()))
 
     total_gt_list = []
     total_gt_list.extend(repeat(ground_truth.flatten(), total))
@@ -254,22 +156,6 @@ def evaluate_results():
 
     print("Calculating Performance")
 
-    # dtn, dfp, dfn, dtp = generate_confusion_matrix(day_gt, day)
-    # ntn, nfp, nfn, ntp = generate_confusion_matrix(night_gt, night)
-
-    # day_classification_report = generate_classification_report(day_gt, day)
-    # print("Day Classification Report\n", day_classification_report)
-
-    # night_classification_report = generate_classification_report(night_gt, night)
-    # print("Night Classification Report\n", night_classification_report)
-
-    # total = np.concatenate((day.flatten(), night.flatten()))
-    # total_gt = np.concatenate((day_gt.flatten(), night_gt.flatten()))
-
-    # ttn, tfp, tfn, ttp = generate_confusion_matrix(total_gt, total)
     total_classification_report = generate_classification_report(total_gt, total)
 
-    # print(f'Days: {days} - TN:{dtn} FP:{dfp} FN:{dfn} TP:{dtp}')
-    # print(f'Night: {nights} - TN:{ntn} FP:{nfp} FN:{nfn} TP:{ntp}')
-    # print(f'Total - TN:{ttn} FP:{tfp} FN:{tfn} TP:{ttp}')
     print("Total Classification Report\n", total_classification_report)
--- src/sky_detector.py
+++ src/sky_detector.py
@@ -29,10 +29,6 @@ def sobel(img):
 
     combined_mag = cv2.addWeighted(sobel_hori_mag, 0.5, sobel_vert_mag, 0.5, 0)
 
-    cv2.imwrite("./report/sobel_hori.png", sobel_hori_mag)
-    cv2.imwrite("./report/sobel_vert.png", sobel_vert_mag)
-    cv2.imwrite("./report/sobel_combined.png", combined_mag)
-
     return combined_mag
 
 
@@ -89,7 +85,6 @@ def initial_segment_sky(img, display=False):
 
     # Get boundary threshold
     boundary_threshold, _ = calculate_boundary_threshold(preprocessed_img, row_percentage, day_night_threshold, day_night_multi)
-    # print(f"Boundary threshold: {boundary_threshold}")
 
     # Apply Sobel edge detection
     sobel_img = sobel(preprocessed_img)
@@ -101,7 +96,6 @@ def initial_segment_sky(img, display=False):
 
     # Generate mask
     mask = generate_mask(sobel_img, boundary_points)
-    # print(set(mask.flatten()))
 
     # Segment sky
     mask = cv2.cvtColor(mask, cv2.COLOR_GRAY2BGR)
@@ -112,114 +106,3 @@ def initial_segment_sky(img, display=False):
         utils.plot_subplots((2, 2), [img, sobel_img, mask, sky_img], ["Original Image", "Sobel Edges", "Sky Segmentation Mask", "Sky Image"])
 
     return mask, sky_img
-
-
-def z_score(score, mean, std):
-    return (score - mean) / (5 * std)
-    # return (score - mean) / std
-
-
-def split_channels(img, coords):
-    """
-    Split the image into three channels
-    """
-    b, g, r = cv2.split(img)
-
-    b_values, g_values, r_values = [], [], []
-
-    for coord in coords:
-        b_values.append(b[coord[0], coord[1]])
-        g_values.append(g[coord[0], coord[1]])
-        r_values.append(r[coord[0], coord[1]])
-
-    return b_values, g_values, r_values
-
-
-def calculate_stats(channel_pixels):
-    """
-    Calculate the mean and standard deviation
-    """
-    b, g, r = channel_pixels
-
-    return (np.mean(b), np.std(b)), (np.mean(g), np.std(g)), (np.mean(r), np.std(r))
-
-
-def calculate_color_probability(pixel, stats):
-    b, g, r = pixel
-    # print(f"b: {b}, g: {g}, r: {r}")
-
-    b_stats, g_stats, r_stats = stats
-
-    b_score = z_score(b, b_stats[0], b_stats[1])
-    g_score = z_score(g, g_stats[0], g_stats[1])
-    r_score = z_score(r, r_stats[0], r_stats[1])
-
-    # print(f"b: {b_score}, g: {g_score}, r: {r_score}")
-    
-    # probability = b_score + g_score + r_score
-    probability = exp(-1 * ( b_score ** 2 + g_score ** 2 + r_score ** 2 ))
-    # print(probability)
-
-    # print(probability)
-
-    return probability
-
-
-def generate_probability_mask(img):
-    """
-    Generate a mask based on the probability of a pixel being a sky pixel
-    """
-    # Variables for probability masking
-    probability_threshold = config.probability_threshold
-
-    # Apply initial segmentation
-    mask, sky = initial_segment_sky(img)
-    mask = cv2.cvtColor(mask, cv2.COLOR_BGR2GRAY)
-
-    # Split the initial sky pixels into channels and calculate their stats
-    channel_pixels = split_channels(sky, np.transpose(np.where(mask == 255)))
-    stats = calculate_stats(channel_pixels)
-
-    # Generate probability mask
-    probability_mask = np.zeros(img.shape, dtype=np.uint8)
-
-    for row in range(img.shape[0]):
-        for col in range(img.shape[1]):
-            if calculate_color_probability(img[row, col], stats) > probability_threshold:
-                probability_mask[row, col] = 255
-    
-    return probability_mask, mask
-
-
-def postprocess(mask, se, iterations=1):
-    """
-    Postprocess the mask, using morphological operations like closing
-    """
-    return cv2.morphologyEx(mask, cv2.MORPH_OPEN, se, iterations=iterations)
-
-
-def segment_sky(img, display=False):
-    """
-    Segment the sky using the probability mask and initial sky segmentation
-    """
-    # Apply probability mask
-    probability_mask, initial_mask = generate_probability_mask(img)
-
-    # Postprocess the mask
-    probability_mask = postprocess(probability_mask, cv2.getStructuringElement(cv2.MORPH_RECT, (5, 5)), 3)
-
-    # Combine the probability mask with the initial mask
-    # print("Initial mask:", initial_mask.shape)
-    # print("Probability mask:", probability_mask.shape)
-    probability_mask = cv2.cvtColor(probability_mask, cv2.COLOR_BGR2GRAY)
-    final_mask = cv2.bitwise_and(initial_mask, probability_mask)
-
-    # Segment sky
-    final_mask = cv2.cvtColor(final_mask, cv2.COLOR_GRAY2BGR)
-    sky_img = cv2.bitwise_and(img, final_mask)
-
-    # Plot images
-    if display:
-        utils.plot_subplots((3, 2), [img, initial_mask, probability_mask, final_mask, sky_img], ["Original Image", "Initial Mask", "Probability Mask", "Final Mask", "Sky Image"])
-
-    return final_mask, sky_img
